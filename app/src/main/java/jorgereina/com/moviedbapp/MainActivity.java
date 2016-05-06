c03892593c9de5e7ea5be16b0555625d4fca9461
jorgereina1986 <jorgereina1986@gmail.com>
2016-05-06 15:01:31 -0400

    main activity UI finished

--- app/src/main/java/jorgereina/com/moviedbapp/MainActivity.java
+++ app/src/main/java/jorgereina/com/moviedbapp/MainActivity.java
@@ -1,13 +1,18 @@
 package jorgereina.com.moviedbapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
